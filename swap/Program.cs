85b4084e07de26f3fcdbb885e5ea91ea0347d7fe
2001monika <105417181+2001monika@users.noreply.github.com>
2022-08-24 15:30:34 +0530

    Add files via upload

---
+++ swap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class swap
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int t;
+            t = a;
+            a = b;
+            b = t;
+
+        }
+        public static void Main()
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine($"before swap a={a},b={b}");
+            Swap(ref a, ref b);
+            Console.WriteLine($"after swap a={a},b={b}");
+        }
+    }
+}
